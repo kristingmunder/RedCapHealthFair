e8e8188225de168d7fb1a7f6ba085bd39104463a
Kevin Davis <kevincarrolldavis@gmail.com>
2019-11-01 23:29:24 -0400

    renamed library to DataSync

--- scripts/DataSync.py
+++ scripts/DataSync.py
@@ -10,36 +10,50 @@ dir_name = os.path.dirname(full_file_path)
 CONFIG_FILE_PATH = f'{dir_name}/../config.json'
 DATA_DICTIONARY_FILE_PATH = f'{dir_name}/../DataDictionary.csv'
 
-def load_config():
+
+def load_config() -> dict:
     print("Loading Configuration file...")
     with open(CONFIG_FILE_PATH, 'r') as config_file:
-        config_text = config_file.read()
-    
-    config = json.loads(config_text)
+        config_text: str = config_file.read()
+
+    config: dict = json.loads(config_text)
     return config
 
 
-def json_to_csv(json_text):
-    json_data = json.loads(json_text)
-    csv_name = DATA_DICTIONARY_FILE_PATH
+def json_to_csv(
+        json_text,
+        file_name=DATA_DICTIONARY_FILE_PATH
+        ) -> pd.DataFrame:
+    """The reason we have this function is so that we can preserve the order of
+    names within the json data. The pandas.to_cvs doesn't allow users to
+    disable sorting"""
+
+    json_data: dict = json.loads(json_text)
+    csv_name: str = file_name
     with open(csv_name, 'w') as f:
         writer = csv.writer(f)
-        keys = []
+        keys: list = []
         for key in json_data[0]:
             keys.append(key)
         writer.writerow(keys)
         for j in json_data:
-            values = []
+            values: list = []
             for key in j:
                 values.append(j[key])
             writer.writerow(values)
-        return pd.read_csv(csv_name)
 
-def upload_data():
+    pd_data = pd.read_csv(csv_name)
+    os.remove(file_name)
+
+    return pd_data
+
+
+def upload_to_test_project():
     data = pd.read_csv(DATA_DICTIONARY_FILE_PATH)
     config = load_config()
     redcap_url = 'https://redcap.miami.edu/api/'
 
+    project_name = "Med IT Test Project"
     request_data = {
             'token': config['test_token'],
             'content': 'metadata',
@@ -47,15 +61,18 @@ def upload_data():
             'data': data.to_json(orient='records'),
             'returnFormat': 'json'
             }
-    
-    print("Attempting to upload the data dictionary...")
+
+    print(f"Attempting to upload the data dictionary to the {project_name}...")
     response = requests.post(redcap_url, request_data)
     print(response.text)
-    
-def download_data_dictionary(main=False):
+
+
+def download_main_project(main=False) -> pd.DataFrame:
+    """This should only be used initially to synchronize to the main project"""
     config = load_config()
     redcap_url = 'https://redcap.miami.edu/api/'
 
+    project_name = "New REDCap" if main is True else "MedIT Test project"
     base = config['test_token'] if main is False else config['main_token']
     request_data = {
             'token': base,
@@ -63,18 +80,19 @@ def download_data_dictionary(main=False):
             'format': 'json',
             'returnFormat': 'json'
             }
-    
-    print("Downloading dictionary...")
+
+    print(f"Downloading dictionary from {project_name}...")
     response = requests.post(redcap_url, request_data)
 
-    json_to_csv(response.text)
-    print("Done")
+    return pd.read_json(response.text)
 
 
 def upload_data_dictionary():
+    config = load_config()
     data = pd.read_csv(DATA_DICTIONARY_FILE_PATH)
     redcap_url = 'https://redcap.miami.edu/api/'
 
+    project_name = "Med IT Test project"
     request_data = {
             'token': config['test_token'],
             'content': 'metadata',
@@ -82,8 +100,7 @@ def upload_data_dictionary():
             'data': data.to_json(orient='records'),
             'returnFormat': 'json'
             }
-    
-    print("Attempting to upload the data dictionary...")
+
+    print(f"Attempting to upload the data dictionary to {project_name}...")
     response = requests.post(redcap_url, request_data)
     print(response.text)
-    
