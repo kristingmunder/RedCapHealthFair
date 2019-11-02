d7952e84ba9a7db8dd646e2373159aba612501ae
Kevin Davis <kevincarrolldavis@gmail.com>
2019-11-02 18:26:29 -0400

    Adding Download Script

--- scripts/download.py
+++ scripts/download.py
@@ -1,3 +1,20 @@
-import DataSync
+"""
+If you prefer to make edits to the data dictionary and the instruments on
+REDCaps designer, you can run the script to pull down the test project
+instruments and update you current CSV files
+"""
 
-DataSync.download_main_project(True)
+import pandas as pd
+import DataSync as ds
+
+from typing import List
+
+
+data_dictionary_df: pd.DataFrame = ds.download_data_df(main=True)
+
+
+instrument_dfs: List[pd.DataFrame] = ds.split_instrument_dataframe(
+        data_dictionary_df
+        )
+
+ds.save_instrument_lists(instrument_dfs)
