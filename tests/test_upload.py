61c275ae0942a71efd22e34433b650c8d1eb6d51
Kevin Davis <kevincarrolldavis@gmail.com>
2019-11-07 10:34:25 -0500

    Adding the upload test This will upload every commit to MedIT Project!

---
+++ tests/test_upload.py
@@ -0,0 +1,20 @@
+"""
+This file will actually test your data upload to ensure that you data
+dictionary edits cause any problems
+"""
+
+import requests
+import pandas as pd
+import scripts.DataSync as ds
+
+from typing import List
+
+
+def test_upload():
+
+    # Obtain our data dictionary
+    instrument_dfs: List[pd.DataFrame] = ds.read_json_instruments_df()
+    data_dictionary: pd.DataFrame = ds.compile_instrument_dataframes(instrument_dfs)
+    response: requests.Response = ds.upload_data_df(data_dictionary)
+
+    assert response.status_code == 200
