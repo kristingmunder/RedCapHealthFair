716ba79e8d5be4a3f7add8143dab606e404d136b
Kevin Davis <kevincarrolldavis@gmail.com>
2019-11-07 10:32:43 -0500

    update the upload script

--- scripts/upload.py
+++ scripts/upload.py
@@ -1,3 +1,10 @@
-import lib
+"""
+This file is for editors to load thier currently saved json files into the Med
+IT test project to begin editing
+"""
 
-lib.upload_data_dictionary()
+import scripts.DataSync as ds
+
+instruments = ds.read_json_instruments_df()
+data_dictionary = ds.compile_instrument_dataframes(instruments)
+ds.upload_data_df(data_dictionary, force_test=True)
