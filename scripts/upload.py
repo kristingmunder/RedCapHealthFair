33c7db4bbe37154aba7f976bf4988080aa8374c2
Kevin Davis <kevincarrolldavis@gmail.com>
2019-11-11 20:19:16 -0500

    fixed import error

--- scripts/upload.py
+++ scripts/upload.py
@@ -3,7 +3,7 @@ This file is for editors to load thier currently saved json files into the Med
 IT test project to begin editing
 """
 
-import scripts.DataSync as ds
+import DataSync as ds
 
 instruments = ds.read_json_instruments_df()
 data_dictionary = ds.compile_instrument_dataframes(instruments)
